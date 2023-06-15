ad6aff696998e35a0fb63b66eb29a0aeb63b3d6e
Cyclone012 <129366524+Cyclone012@users.noreply.github.com>
2023-06-15 22:02:51 +0700

    Delete Script directory

--- Script/script.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll(".carousel-item").forEach(el => {
-    el.addEventListener("scroll", () => {
-        if (el.getBoundingClientRect().top < window.innerHeight) {
-            el.querySelector("img").src = el.dataset.src;
-        }
-    });
-});
