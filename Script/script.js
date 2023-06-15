90b05dfaa413eae09a6cde4e5bd2c51d2f08d53e
Cyclone012 <129366524+Cyclone012@users.noreply.github.com>
2023-06-15 22:40:53 +0700

    Add files via upload

--- Script/script.js
+++ Script/script.js
@@ -1,7 +1,11 @@
-document.querySelectorAll(".carousel-item").forEach(el => {
-    el.addEventListener("scroll", () => {
-        if (el.getBoundingClientRect().top < window.innerHeight) {
-            el.querySelector("img").src = el.dataset.src;
-        }
+if ('loading' in HTMLImageElement.prototype) {
+    const images = document.querySelectorAll('img[loading="lazy"]');
+    images.forEach(img => {
+        img.src = img.dataset.src;
     });
-});
+} else {
+    // Dynamically import the LazySizes library
+    const script = document.createElement('script');
+    script.src = 'https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.1.2/lazysizes.min.js';
+    document.body.appendChild(script);
+}
